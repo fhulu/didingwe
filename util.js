efd6c2a35e942a1bc9eca5caec8229e0796f4a78
Fhulu Lidzhade <fhulu@mukoni.co.za>
2018-05-08 09:23:48 +0200

    merge() to first target

--- util.js
+++ util.js
@@ -8,6 +8,7 @@ util.is_string = x => typeof x == "string";
 util.is_array = x => Array.isArray(x);
 util.is_object = x => !util.is_primitive(x) && x == Object(x);
 util.is_iteratable = x => Symbol.iterator in Object(obj);
+util.last = x => x[x.length]
 
 util.clone = (x, option) => {
   if (util.is_primitive(x)) return x;
@@ -20,54 +21,47 @@ util.merge_array = (x,y) => {
   return x===undefined? y : x.concat(y);
 }
 
-function init_merge_options(options) {
-  return Object.assign({
+util.merge = (target, ...sources) => {
+  var options = {
     clone: true,
     is_mergeable: a=>true,
     array_merger: util.merge_array
-  }, options)
-}
+  };
 
-util.merge = (x, y, options) => {
+  var last = util.last(sources)
+  if (typeof last == 'function') {
+    Object.assign(options, last());
+    sources.pop();
+  }
 
   function doit(x, y) {
     // if (!options.is_mergeable(y)) return clone(y);
-
+    if (!y) return x;
     var is_array = util.is_array(x);
     var same_type = is_array === util.is_array(y);
     if (!same_type) return util.clone(y);
     if (is_array) return options.array_merger(x,y);
 
-    var r = {};
-    for (var key in x) {
-      if (!x.hasOwnProperty(key)) continue;
-      var vy = y[key];
-      if (util.is_primitive(vy)) {
-        r[key] = util.clone(vy);
-        continue;
-      }
-
-      var vx = x[key];
-      if (vy === undefined) {
-        r[key] = util.clone(vx);
-        continue;
-      }
-
-      r[key] = doit(vx, vy, options);
-    }
-
     for (var key in y) {
-      if (!(key in r)) r[key] = util.clone(y[key]);
-    }
-    return r;
-  }
-  options = init_merge_options(options);
-  return doit(x,y);
-}
+      if (!y.hasOwnProperty(key)) continue;
+      var vx = x[key];
+      var vy = y[key];
+      if (!vx || util.is_primitive(vy)) {
+        x[key] = util.clone(vy);
+        continue;
+      }
 
-util.merge.all = (array, options) => {
-  options = init_merge_options(options);
-  return array.reduce((prev, next, init)=>merge(prev, next, options))
+      x[key] = doit(x[key], vy);
+    }
+
+    return x;
+  }
+
+  for (var source of sources) {
+    doit(target, source);
+  }
+
+  return target;
 }
 
 util.walk = (obj, callback) => {
