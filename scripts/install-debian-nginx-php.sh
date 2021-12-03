eeb17c1c2c65d7da45f750fe3975f48f501662be
Fhulu Lidzhade <fhulu.lidzhade@metacom.net>
2021-12-03 19:56:12 +0200

    added install scripts for nginx on debian

---
+++ scripts/install-debian-nginx-php.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo ""
+echo "Didingwe NGINX PHP Installer"
+echo ""
+
+default_sites_dir=/etc/nginx/sites-enabled
+script_source_dir=$(dirname $BASH_SOURCE)
+sample_web_server_conf=$script_source_dir/sample-nginx.conf
+web_server_conf_dir=nginx
+export default_sites_dir sample_web_server_conf web_server_conf_dir
+$script_source_dir/install-debian-php.sh
+
+# Reload php-fpm
+sudo service php7.4-fpm reload
+
+# Reload nginx with new site
+sudo service nginx reload
+
+echo ""
+echo "Installation complete."
+echo ""
+
+exit 0
+
