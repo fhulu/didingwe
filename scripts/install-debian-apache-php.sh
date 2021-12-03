1ed47d9c3c9809f24b080a9174c00e79928c1512
Fhulu Lidzhade <fhulu.lidzhade@metacom.net>
2021-12-03 17:42:41 +0200

    refactor installation script to prep for different web servers

---
+++ scripts/install-debian-apache-php.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo ""
+echo "Didingwe Apache PHP Installer"
+echo ""
+
+default_sites_dir=/etc/apache2/sites-enabled
+script_source_dir=$(dirname $BASH_SOURCE)
+sample_web_server_conf=$script_source_dir/sample-apache2.conf
+web_server_conf_dir=apache2
+export default_sites_dir sample_web_server_conf web_server_conf_dir
+bash -x $script_source_dir/install-debian-php.sh
+
+# Link PHP rewrite rules and enable Apache Rewrite module
+ln -s dididngwe-php/.htaccess -t web
+sudo a2enmod rewrite
+
+# Reload apache with new settings
+sudo service apache2 reload
+
+echo ""
+echo "Installation complete."
+echo ""
+
+exit 0
+
--- scripts/install-debian-php.sh
+++ scripts/install-debian-php.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-echo ""
-echo "Didingwe Apache PHP Installer"
-echo ""
-
 default_app_dir=`pwd`
 dest_dir=$1
 app_dir=${dest_dir:-$default_app_dir}
 cd $app_dir
 project_name="$(basename $app_dir)"
+didi_dir=didingwe
 
-# Configure domain name to access via web port 80, eg. http://example.local
+# Configure domain name to access, eg. http://example.local
 default_domain_name=$project_name.local
 read -r -p "Enter web domain name [$default_domain_name]: " domain_name
 domain_name=${domain_name:-$default_domain_name}
 
-read -r -p "Would you like to add this name to your local hosts file [N]: " add_host
+# Configure web port, eg. http://example.local:80
+default_port=80
+read -r -p "Enter web domain port [$default_port]: " port
+port=${port:-$default_port}
+
+read -r -p "Would you like to add this web domain name to your local hosts file [N]: " add_host
 add_host=${add_host:-N}
 if [[ "$add_host" =~ ^[yY](es)?$ ]]; 
 then
@@ -25,20 +27,12 @@ then
     sudo echo "$host_ip $domain_name" >> /etc/hosts
 fi
 
-# Configure Didingwe start point
-default_didi_dir=didingwe
-read -r -p "Enter the directory in which the Didingwe framework resides [$default_didi_dir]: " didi_dir
-didi_dir=${didi_dir:-$default_didi_dir}
-
 # Create required directories
 web_dir=web
 mkdir -p $web_dir
 
 vocab_dir=vocab
 mkdir -p $vocab_dir
-
-apache_dir=$app_dir/apache
-mkdir -p $apache_dir
 
 log_dir=log
 mkdir -p $log_dir
@@ -49,29 +43,27 @@ read -r -p "User name of the web user [$default_web_user]: " web_user
 web_user=${web_user:-$default_web_user}
 sudo chown $web_user:$web_user $log_dir
 
-# Configure Apache specific settings
-apache_conf="$apache_dir/$project_name.conf"
+# Configure Web server specific settings
+web_server_conf_dir=$app_dir/$web_server_conf_dir
+mkdir -p $web_server_conf_dir
+web_server_conf="$web_server_conf_dir/$project_name.conf"
 site_dir=${app_dir//\//\\/}
-sed "s/\$site_dir/${site_dir}/g; s/\$domain_name/${domain_name}/g" $didi_dir/apache/apache.conf >$apache_conf
-default_sites_dir=/etc/apache2/sites-enabled
-read -r -p "Enter Apache2 sites-enabled directory [$default_sites_dir]: " sites_dir
+sed "s/\$site_dir/${site_dir}/g; s/\$domain_name/${domain_name}/g; s/\$port/${port}/g" $sample_web_server_conf >$web_server_conf
+read -r -p "Enter sites-enabled directory [$default_sites_dir]: " sites_dir
 sites_dir=${sites_dir:-$default_sites_dir}
-sudo ln -s $apache_conf $sites_dir/$project_name.conf
-sudo a2enmod rewrite
+mkdir -p $sites_dir
+sudo ln -s $web_server_conf $sites_dir/$project_name.conf
 
 # Install PHP
 sudo apt install php7.4 php7.4-yaml
 
 # Get PHP repostory
-git clone https://github.com/fhulu/didingwe-php.git $php_dir
 php_dir=didingwe-php
 mkdir -p $php_dir
+git clone https://github.com/fhulu/didingwe-php.git $php_dir
 
 # Link PHP index page
 ln -s ../$php_dir/index.php -t $web_dir
-
-# Link PHP rewrite rules
-ln -s ../$php_dir/.htaccess -t $web_dir
 
 # Link Framework Web Assets 
 ln -s ../$didi_dir/web $web_dir/didi
@@ -86,13 +78,6 @@ sed "s/\$didi_root/${didi_root}/g; s/\$project_name/${project_name}/g; s/\$proje
 
 # Sample site-specific configuration file
 sed "s/\$project_name/${project_name}/g" $didi_dir/vocab/sample-site-config.yml >$app_dir/vocab/.site-config.yml
-
-# Reload apache with new settings
-sudo service apache2 reload
-
-echo ""
-echo "Installation complete."
-echo ""
 
 exit 0
 
